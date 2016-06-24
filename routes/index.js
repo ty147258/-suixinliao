3bd4221180ee8dd47d463416d8481241b6e51f09
lzy <148334733@qq.com>
2016-06-24 15:43:54 +0800

    新增标签

--- routes/index.js
+++ routes/index.js
@@ -149,7 +149,8 @@ module.exports = function(app){
     app.post('/post', checkLogin);
     app.post('/post',function(req,res){
         var currentUser = req.session.user;
-        var post = new Post(currentUser.name,req.body.title,req.body.post);
+        var tags = [req.body.tag1, req.body.tag2, req.body.tag3];
+        var post = new Post(currentUser.name, req.body.title, tags, req.body.post);
         post.save(function(err){
             if (err) {
                 req.flash('error', err);
@@ -190,6 +191,38 @@ module.exports = function(app){
             }
             res.render('archive', {
                 title: '存档',
+                posts: posts,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
+    });
+    //标签
+    app.get('/tags', function (req, res) {
+        Post.getTags(function (err, posts) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('tags', {
+                title: '标签',
+                posts: posts,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
+    });
+    //拥有特定标签的页面
+    app.get('/tags/:tag', function (req, res) {
+        Post.getTag(req.params.tag, function (err, posts) {
+            if (err) {
+                req.flash('error',err);
+                return res.redirect('/');
+            }
+            res.render('tag', {
+                title: 'TAG:' + req.params.tag,
                 posts: posts,
                 user: req.session.user,
                 success: req.flash('success').toString(),
