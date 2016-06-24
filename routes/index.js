a7a7c536c14ef7b4f5abb672ca8b70644d96974c
lzy <148334733@qq.com>
2016-06-24 15:03:58 +0800

    添加存档功能

--- routes/index.js
+++ routes/index.js
@@ -181,6 +181,22 @@ module.exports = function(app){
         req.flash('success', '文件上传成功!');
         res.redirect('/upload');
     });
+    //存档
+    app.get('/archive', function (req, res) {
+        Post.getArchive(function (err, posts) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('archive', {
+                title: '存档',
+                posts: posts,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
+    });
     //用户文章
     app.get('/u/:name', function (req, res) {
         var page = parseInt(req.query.p) || 1;
