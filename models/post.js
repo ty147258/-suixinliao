413a25eb83bcc4da25599aca2dc59593084a6c1a
lzy <148334733@qq.com>
2016-06-24 14:38:39 +0800

    添加留言功能

--- models/post.js
+++ models/post.js
@@ -26,11 +26,12 @@ Post.prototype.save = function(callback){
     }
     //整理下要存入数据库的数据
     var post = {
-        name:this.name,
-        time:time,
+        name: this.name,
+        time: time,
         title:this.title,
-        post:this.post
-    }
+        post: this.post,
+        comments: []
+    };
     //打开数据库
     mongodb.open(function(err,db){
         if(err){
@@ -108,8 +109,13 @@ Post.getOne = function(name, day, title, callback) {
                 if (err) {
                     return callback(err);
                 }
-                //解析 markdown 为 html
-                doc.post = markdown.toHTML(doc.post);
+                //修改为循环留言内容
+                if (doc) {
+                    doc.post = markdown.toHTML(doc.post);
+                    doc.comments.forEach(function (comment) {
+                        comment.content = markdown.toHTML(comment.content);
+                    });
+                }
                 callback(null, doc);//返回查询的一篇文章
             });
         });
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var crypto = require('crypto');
 var User = require('../models/user.js');
 var Post = require('../models/post.js');
-
+var Comment = require('../models/comment.js');
 
 var multer  = require('multer');
 //新的使用方法配置multer
@@ -213,6 +213,28 @@ module.exports = function(app){
                 success: req.flash('success').toString(),
                 error: req.flash('error').toString()
             });
+        });
+    });
+    //留言
+    app.post('/u/:name/:day/:title', function (req, res) {
+        var date = new Date(),
+            time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+                date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+        var comment = {
+            name: req.body.name,
+            email: req.body.email,
+            website: req.body.website,
+            time: time,
+            content: req.body.content
+        };
+        var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+        newComment.save(function (err) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('back');
+            }
+            req.flash('success', '留言成功!');
+            res.redirect('back');
         });
     });
     //编辑文章
